72bd718226d441640afc6220d6c39fd717e6affc
Patrick <patrickgmartinez@hotmail.com>
2024-09-23 11:07:43 -0300

    Atualização na Leitura do Script

--- Senha.py
+++ Senha.py
@@ -13,6 +13,9 @@ def open_site_and_send_f9():
     chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
     chrome_options.add_argument("--kiosk-printing")  # Ativa a impressão em modo quiosque
     chrome_options.add_argument("--window-size=1920x1080")  # Define o tamanho da janela (opcional)
+    
+    # Coloca o navegador fora da tela (simulando "headless")
+    chrome_options.add_argument("--window-position=-10000,0")  # Posiciona a janela fora da tela visível
 
     # Usando o ChromeDriverManager para baixar e configurar o ChromeDriver automaticamente
     service = Service(ChromeDriverManager().install())
@@ -24,7 +27,7 @@ def open_site_and_send_f9():
     # Enviar o comando F9 para a página
     body = driver.find_element("tag name", 'body')
     body.send_keys(Keys.F9)
-    print("Comando F9 enviado!")
+    print("Processo de impressão iniciado...")
 
     # Esperar um tempo antes de fechar (por exemplo, 10 segundos)
     print("Aguardando Impressão...")
